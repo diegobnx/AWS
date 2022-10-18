1bb525f1785d8249644ac59abef3d1876d413437
diegobnx <diego.dmnetwork@gmail.com>
2022-10-18 19:08:18 -0300

    Adicionado funções e validações para keys e pw ativos

--- iamMonv2.py
+++ iamMonv2.py
@@ -37,32 +37,70 @@ def days_till_expire(last_changed, max_age):
 
 
 for user in resource.users.all():
+
     keys_response = client.list_access_keys(UserName=user.user_name)
     last_access = None
     max_age = get_max_password_age(client)
+
     login_profile = user.LoginProfile()
-    password_expires = days_till_expire(login_profile.create_date, max_age)
-    accesskeydate = keys_response['AccessKeyMetadata'][0]['CreateDate'].date()
-    currentdate = date.today()
-    active_days = currentdate - accesskeydate
+    try:
+        password_expires = days_till_expire(login_profile.create_date, max_age)
+    except:
+        print('Não existe password ativo para o usuario: ', user.user_name)
 
-    for key in keys_response['AccessKeyMetadata']:
-        if active_days.days <= 10:
-            client.update_access_key(
-                AccessKeyId=key['AccessKeyId'], Status='Inactive', UserName=user.user_name)
-        if password_expires <= 86:
-            login_profile.delete()
+    keys = list(resource.User(user.user_name).access_keys.all())
 
-    if active_days.days >= 0:
-        final_report_all_key += str(number) + " - Username: " + \
-            [user.user_name][0] + " - AccessKey Age: " + \
-            str(active_days.days) + " dias \n"
+    for i in range(len(keys)):
+        key = keys[i].id
+        last_used_keys = client.get_access_key_last_used(AccessKeyId=key)
 
-    if password_expires >= 0:
-        final_report_console += str(number) + " - Username: " + [
-            user.user_name][0] + " - Console passwords expira em: " + str(password_expires) + " dias \n"
+        if 'LastUsedDate' in last_used_keys['AccessKeyLastUsed']:
+            accesskey_last_used = last_used_keys['AccessKeyLastUsed']['LastUsedDate']
+            if last_access is None or accesskey_last_used < last_access:
+                last_access = accesskey_last_used
+
+        accesskeydate = keys_response['AccessKeyMetadata'][i]['CreateDate'].date(
+        )
+        currentdate = date.today()
+        active_days = currentdate - accesskeydate
+
+        keyStatus = keys_response['AccessKeyMetadata'][i]['Status']
+
+        if i > 1:
+            if active_days.days >= 0:
+                final_report_all_key += str(number) + ". Status: " + "[" + keyStatus + "]" + " - Username: " + \
+                    [user.user_name][i] + " - AccessKey Age: " + \
+                    str(active_days.days) + " dias \n"
+                
+        else:
+            if active_days.days >= 0:
+                final_report_all_key += str(number) + ". Status: " + "[" + keyStatus + "]" + " - Username: " + \
+                    [user.user_name][0] + " - AccessKey Age: " + \
+                    str(active_days.days) + " dias \n"
+
+        # if active_days.days <= 90 and last_access is None:
+        #     client.update_access_key(
+        #         AccessKeyId=key, Status='Inactive', UserName=user.user_name)
+
+        if 'password_expires' in globals() and 'password_expires' in locals():
+            # if password_expires <= 90:
+            #     login_profile.delete()
+            number -=1
+            if password_expires >= 0:
+                final_report_console += str(number) + ". Username: " + [
+                    user.user_name][0] + " - Console passwords expira em: " + str(password_expires) + " dias \n"
+                number += 1
+
+    if last_access is not None:
+        #delta = (today - last_access.replace(tzinfo=None)).days
+        if active_days.days >= 0:
+            final_report_key += str(number) + ". Status: " + "[" + keyStatus + "]" + " - Username: " + \
+                [user.user_name][0] + " - Idade da AccessKey que possui acesso: " + \
+                str(active_days.days) + " days"
+            
 
     number += 1
 
 print(final_report_console)
 print(final_report_all_key)
+print(final_report_key)
